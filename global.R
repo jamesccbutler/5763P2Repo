9857888ff5f28d334b815ded2aa84cdb9031d798
xu-zihan <zx24@st-andrews.ac.uk>
2019-10-22 16:18:13 +0100

    adding api info

--- global.R
+++ global.R
@@ -1,2 +1,12 @@
 library(shiny)
 library(shinydashboard)
+library(twitteR)
+library(DT)
+
+consumer_key <- "Eo0siVEkFYJyC0bJiKjUiKaKW"
+consumer_secret <- "Tt3wmRQZSCKlLmaqsmlYXwSec8RQYmZxoqxJGfM3nC2tWnwPa1"
+access_token <- "1367409132-HW02jLOJ7ISs3hFmHeXaeYbIxcYFUT5XHhiKVcB"
+access_secret <- "FyzY40plw4nbc68G0PgbL4chFWV8ETVRw4G2OI8Cumzb1" 
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
+
